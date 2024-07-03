e08414e491b04021643c968cfddb7da30af52e93
Pablo Nudenberg <pablonudenberg@gmail.com>
2024-07-03 09:45:23 -0300

    create react app

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+ 
+
+  return (
+    <>  
+
+      <div className=" flex flex-col gap-[5px] px-20 py-40">
+        <p className="text-[blue]">Probando</p>
+          <Button className= "bg-black w-[80px]"> Hola</Button>
+
+      </div>
+        
+    </>
+  )
+}
+
+export default App
