b5c8e0b9e79cd52987750f60dbb9d07f5701c2f4
SnackMaste <jcgarcia1565@misena.edu.co>
2024-07-17 13:15:58 -0500

    feat: :sparkles: creacion y ajuste de los componentes de Tables
    se ajustaron los componentes de tables para que tenga una modificacion mas versatil y que se puedan ajustar tanto a los requerimientos de los clientes como al de los admins

--- Frontend/client/src/components/table/4Chairs.jsx
+++ Frontend/client/src/components/table/4Chairs.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const TableFourChairs = () => {
+const TableFourChairs = ({name, id, reservedChairs}) => {
+
+    const defaultChairsColor = "bg-[#58575776]";
+    const reservedChairsColor = "bg-[#7F2A4490]";
+
     return (
-        <div className=" mt-10 w-[250px] h-[220px]">
+        <div id={id} className=" m-10 w-[250px] h-[220px]">
             <div className="h-full flex flex-col justify-between">
                 <div className="flex justify-around">
-                    <div className="relative w-[74px] h-[74px] rounded-full bg-[#58575776]"></div>
-                    <div className="relative w-[74px] h-[74px] rounded-full bg-[#7F2A4490]"></div>
+                    <div className={`relative w-[74px] h-[74px] rounded-full ${reservedChairs === 4 ? reservedChairsColor : defaultChairsColor}`}></div>
+                    <div className={`relative w-[74px] h-[74px] rounded-full ${reservedChairs === 3 || reservedChairs === 4 ? reservedChairsColor : defaultChairsColor}`}></div>
                 </div>
                 <div className="flex justify-around">
-                    <div className="relative w-[74px] h-[74px] rounded-full bg-[#7F2A4490]"></div>
-                    <div className="relative w-[74px] h-[74px] rounded-full bg-[#7F2A4490]"></div>
+                    <div className={`relative w-[74px] h-[74px] rounded-full ${reservedChairs === 3 || reservedChairs === 4 ? reservedChairsColor : defaultChairsColor}`}></div>
+                    <div className={`relative w-[74px] h-[74px] rounded-full ${reservedChairs === 3 || reservedChairs === 4 ? reservedChairsColor : defaultChairsColor}`}></div>
                 </div>
             </div>
             <div className="bg-[#C0C0C030] w-[240px] h-[140px] border-solid rounded-xl cursor-pointer relative top-[-185px] backdrop-blur border-4 border-[#343333] hover:scale-110">
                 <div className="w-full h-full flex justify-center items-center">
-                    <span className="text-xl font-title font-bold">MESA 1</span>
+                    <span className="text-xl font-title font-bold text-color-secondary">{name}</span>
                 </div>
             </div>
         </div>
