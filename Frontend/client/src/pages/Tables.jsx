ee79893ced9e598ac65557100a00b5327db65e59
SnackMaste <jcgarcia1565@misena.edu.co>
2024-07-18 21:01:47 -0500

    feat: :sparkles: Creacion de los modales e integracion del crud Tables
    se creo la estructura y los modales, se añadieron para cada ocasion de requirimiento y se creo la funcionalidad de los mismo y la estructura de los formularios

--- Frontend/client/src/pages/Tables.jsx
+++ Frontend/client/src/pages/Tables.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useTablesStore from "@/stores/useTableStore";
 import TableFourChairs from "@/components/table/4Chairs";
 import TableTwoChairs from "@/components/table/2Chairs";
 import TableSixChairs from "@/components/table/6Chairs";
+import NewMesa from "@/components/modal/newTable";
+import EditMesa from "@/components/modal/editTable";
+import Button1 from "@/components/ui/button1";
 
 const Tables = () => {
   const { tables, error, loading, fetchTables } = useTablesStore();
@@ -11,6 +14,23 @@ const Tables = () => {
     fetchTables();
   }, [fetchTables]);
 
+  const [isModalOpenNewMesa, setIsModalOpenNewMesa] = useState(false);
+  const [isModalOpenEditMesa, setIsModalOpenEditMesa] = useState(false);
+  const [idMesa, setIdMesa] = useState(null);
+  const [nameMesa, setNameMesa] = useState(null);
+  const [capacityMesa, setCapacityMesa] = useState(null);
+
+  const openModalNewMesa = () => setIsModalOpenNewMesa(true);
+  const closeModalNewMesa = () => setIsModalOpenNewMesa(false);
+  const openModalEditMesa = () => setIsModalOpenEditMesa(true);
+  const closeModalEditMesa = () => setIsModalOpenEditMesa(false);
+
+  const dataMesa = (id, name, capacity) => {
+    openModalEditMesa();
+    setIdMesa(id);
+    setNameMesa(name);
+    setCapacityMesa(capacity);
+  }
   if (loading) {
     return (
       <div className="flex justify-center w-full bg-color-bg h-[10vh]">
@@ -38,24 +58,31 @@ const Tables = () => {
   const tablesResponse = tables["hydra:member"].map((table) => {
     if (table["capacity"] === 2) {
       return (
-        <TableTwoChairs name={table.name} id={table.id} reservedChairs={1} />
+        <TableTwoChairs key={table.id} name={table.name} id={table.id} hover onClick={() => dataMesa(table.id, table.name, table.capacity)} />
       );
     }
     if (table["capacity"] === 4) {
       return (
-        <TableFourChairs name={table.name} id={table.id} reservedChairs={3} />
+        <TableFourChairs key={table.id} name={table.name} id={table.id} hover onClick={() => dataMesa(table.id, table.name, table.capacity)} />
       );
     }
     if (table["capacity"] === 6) {
       return (
-        <TableSixChairs name={table.name} id={table.id} reservedChairs={5} />
+        <TableSixChairs key={table.id} name={table.name} id={table.id} hover onClick={() => dataMesa(table.id, table.name, table.capacity)} />
       );
     }
   });
 
   return (
-    <div className="flex justify-center w-full h-[70vh] bg-color-bg">
-      {tablesResponse}
+    <div className=" w-full h-[70vh] bg-color-bg">
+      <div className="w-full flex justify-center my-4">
+        <Button1 type="button" text="Agregar Una Nueva Mesa" onClick={openModalNewMesa} />
+      </div>
+      <NewMesa isOpen={isModalOpenNewMesa} onClose={closeModalNewMesa} />
+      <div className="flex justify-center">
+        {tablesResponse}
+      </div>
+      <EditMesa isOpen={isModalOpenEditMesa} onClose={closeModalEditMesa} id={idMesa} nameMesa={nameMesa} capacityMesa={capacityMesa}/>
     </div>
   );
 };
