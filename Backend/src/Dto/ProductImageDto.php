d9586c0850f06153d98e2f9d66988caf0b13445a
Nicolas <nicolasgalarza40@gmail.com>
2024-07-22 05:54:16 -0300

    docs: :memo: add documentation

--- Backend/src/Dto/ProductImageDto.php
+++ Backend/src/Dto/ProductImageDto.php
@@ -2,13 +2,18 @@
 
 namespace App\Dto;
 
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use ApiPlatform\Metadata\ApiProperty;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ProductImageDto
-{
+{   
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'string',
+            'format' => 'binary'
+        ]
+    )]
     #[Assert\NotBlank(groups: ['product-image:validation'])]
     #[Assert\File(mimeTypes: ['image/*'], maxSize: '2048k', groups: ['product-image:validation'])]
     #[Groups(['product-image:write'])]
