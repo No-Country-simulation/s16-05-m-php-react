29c78a90a0882aecfdd7a145368acefd77677b8d
Pablo Nudenberg <pablonudenberg@gmail.com>
2024-07-23 02:14:54 -0300

    feat: :sparkles: Landing page + features
    Landing page and Navbar mobile styles created

--- Frontend/client/src/router/Router.jsx
+++ Frontend/client/src/router/Router.jsx
@@ -5,13 +5,17 @@ import Tables from "@/pages/Tables";
 import PrivateRoute from "./PrivateRoom";
 import useAuthStore from "@/stores/useAuthStore";
 import TablesUser from "@/pages/TablesUser";
+import Intro from "@/pages/Intro";
 
 const Router = () => {
   const { token } = useAuthStore();
 
   return (
     <Routes>
-      <Route path="/" element={token ? <Navigate to="/tables" /> : <Login />} />
+      <Route
+        path="/admin"
+        element={token ? <Navigate to="/tables" /> : <Login />}
+      />
       <Route
         path="/tables"
         element={
@@ -21,6 +25,7 @@ const Router = () => {
         }
       />
       <Route path="/reserve" element={<TablesUser />} />
+      <Route path="/" element={<Intro />} />
     </Routes>
   );
 };
