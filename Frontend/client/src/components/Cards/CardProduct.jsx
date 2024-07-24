b796d4ea03a0da182f7a23a77fc33bbb96282d74
SnackMaste <jcgarcia1565@misena.edu.co>
2024-07-24 12:26:12 -0500

    feat: :zap: add style disables cardProduct
    se añadio un estilo el cual permitira mostrar al administrador cuando los productos estan desabilitados

--- Frontend/client/src/components/Cards/CardProduct.jsx
+++ Frontend/client/src/components/Cards/CardProduct.jsx
@@ -4,13 +4,16 @@ import Button1 from "@/components/ui/button1";
 const CardProduct = ({name, description, image, price, disabled}) => {
     const priceFinal = price.toLocaleString('es-ES', {style: 'currency', currency: 'ARS', minimumFractionDigits: 0});
     var disabledStyle = null;
+    var noDisponible = null;
     if(disabled){
-        disabledStyle = (<div className="absolute inset-0 rounded-t-2xl bg-black opacity-70 flex items-center justify-center"><span className="text-3xl font-bold text-red-900 opacity-100 -rotate-45">No Disponible</span></div>);
+        disabledStyle = (<div className="absolute inset-0 rounded-t-2xl bg-black opacity-70 flex items-center justify-center"></div>);
+        noDisponible = (<div className="absolute inset-0 flex items-center justify-center"><span className=" text-3xl font-bold text-red-500 -rotate-45">No Disponible</span></div>);
     }
     return (
         <div className="w-[250px] h-[450px] rounded-2xl bg-[#3C3A3A] m-10">
             <div className="w-[250px] h-[390px] flex flex-col justify-around items-center relative">
                 {disabledStyle}
+                {noDisponible}
                 <img src={image} alt="Imagen de un producto" className="w-[230px] h-[170px] rounded-2xl object-cover object-center"/>
                 <h1 className="text-3xl px-3 font-bold text-color-secondary">{name}</h1>
                 <p className="text-base px-3 font-semibold text-white">{description}</p>
