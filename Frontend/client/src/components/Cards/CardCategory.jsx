91ae994a3bce745517d18bd9211689568f6cc8cb
SnackMaste <jcgarcia1565@misena.edu.co>
2024-07-25 09:09:56 -0500

    feat: ajusted de los componentes
    se ajustaron los componentes CardCategory y Button1 para que funcionen correctamente con los nuevos requerimientos

--- Frontend/client/src/components/Cards/CardCategory.jsx
+++ Frontend/client/src/components/Cards/CardCategory.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Button1 from "@/components/ui/button1";
 
-const CardCategory = ({name, description, image}) => {
-    const products = () => {
-        window.location.pathname = "/products";
+const CardCategory = ({name, description, image, edit, deleteOnClick,editOnClick, notHover, notOnClick, id}) => {
+    var heightDiv = "h-[250px]";
+    var buttons = "";
+    var noHover = false;
+    var onclick = ()=> products(name);
+    if(edit){
+        buttons = (<div className="flex w-full justify-around mt-3">
+            <Button1
+                type="button"
+                text="Eliminar"
+                onClick={deleteOnClick}
+            />
+            <Button1
+                type="button"
+                text="Editar"
+                variant={"confirm"}
+                onClick={editOnClick}
+            />
+        </div>);
+        heightDiv = "h-[310px]";
+    }
+    if(notHover){
+        noHover = true;
+    }
+    if(notOnClick){
+        onclick = () => {};
+    }
+    const products = (category) => {
+        window.location.pathname = `/products/${category}`;
     };    
     return (
-        <div className="bg-[#3C3A3A] w-[250px] h-[310px] rounded-2xl m-10">
-            <div className="w-[250px] h-[250px] rounded-2xl relative"
+        <div className={`bg-[#3C3A3A] w-[250px] ${heightDiv} rounded-2xl m-10`} id={id}>
+            <div className="w-[250px] h-[250px] rounded-2xl relative bg-cover bg-center bg-no-repeat"
             style={{backgroundImage: `url(${image})`}}
             >
                 <div className="absolute inset-0 rounded-2xl bg-black opacity-50"></div>
@@ -17,20 +43,11 @@ const CardCategory = ({name, description, image}) => {
                     <Button1
                         type="button"
                         text="Ver Productos"
-                        onClick={products}
+                        onClick={onclick}
+                        noHover={noHover}
                     />
                 </div>
-                <div className="flex w-full justify-around mt-3">
-                    <Button1
-                        type="button"
-                        text="Eliminar"
-                    />
-                    <Button1
-                        type="button"
-                        text="Editar"
-                        variant={"confirm"}
-                    />
-                </div>
+                {buttons}
             </div>
         </div>
     );
