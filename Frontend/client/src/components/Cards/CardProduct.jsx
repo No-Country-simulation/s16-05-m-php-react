5bdfa15faea50a80d3a0a4819588b5ba61c5c736
SnackMaste <jcgarcia1565@misena.edu.co>
2024-07-26 00:24:18 -0500

    feat: :sparkles: ajustes de CardProduct
    se hicieron ajustes al componente de cardProduct para que sea mas versatil y se ajusten a las necesidades actuales

--- Frontend/client/src/components/Cards/CardProduct.jsx
+++ Frontend/client/src/components/Cards/CardProduct.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import Button1 from "@/components/ui/button1";
 
-const CardProduct = ({name, description, image, price, disabled}) => {
-    const priceFinal = price.toLocaleString('es-ES', {style: 'currency', currency: 'ARS', minimumFractionDigits: 0});
+const CardProduct = ({name, description, image, price, disabled, edit}) => {
+    const priceFinal = price.toLocaleString();
     var disabledStyle = null;
     var noDisponible = null;
-    if(disabled){
-        disabledStyle = (<div className="absolute inset-0 rounded-t-2xl bg-black opacity-70 flex items-center justify-center"></div>);
+    var editProduct = null;
+    var height = "h-[390px]";
+    if(disabled === false) {
+        if(edit){
+            disabledStyle = (<div className="absolute inset-0 rounded-t-2xl bg-black opacity-70 flex items-center justify-center"></div>);
+        }else{
+            disabledStyle = (<div className="absolute inset-0 rounded-2xl bg-black opacity-70 flex items-center justify-center"></div>);
+        }
+        
         noDisponible = (<div className="absolute inset-0 flex items-center justify-center"><span className=" text-3xl font-bold text-red-500 -rotate-45">No Disponible</span></div>);
     }
-    return (
-        <div className="w-[250px] h-[450px] rounded-2xl bg-[#3C3A3A] m-10">
-            <div className="w-[250px] h-[390px] flex flex-col justify-around items-center relative">
-                {disabledStyle}
-                {noDisponible}
-                <img src={image} alt="Imagen de un producto" className="w-[230px] h-[170px] rounded-2xl object-cover object-center"/>
-                <h1 className="text-3xl px-3 font-bold text-color-secondary">{name}</h1>
-                <p className="text-base px-3 font-semibold text-white">{description}</p>
-                <p className="text-lg px-3 font-semibold text-color-secondary">$ {priceFinal}</p>
-            </div>
-            <div className="flex w-full justify-around mt-3">
+    if(edit){
+        editProduct = (<div className="flex w-full justify-around mt-3">
                     <Button1
                         type="button"
                         text="Eliminar"
@@ -29,7 +27,21 @@ const CardProduct = ({name, description, image, price, disabled}) => {
                         text="Editar"
                         variant={"confirm"}
                     />
-                </div>
+            </div>
+        );
+        height = "h-[450px]";
+    }
+    return (
+        <div className={`w-[250px] ${height} rounded-2xl bg-[#3C3A3A] m-10`}>
+            <div className="w-[250px] h-[390px] flex flex-col justify-around items-center relative">
+                {disabledStyle}
+                {noDisponible}
+                <img src={`${image}`} alt="Imagen de un producto" className="w-[230px] bg-color-bg h-[170px] rounded-2xl object-cover object-center"/>
+                <h1 className="text-3xl px-3 font-bold text-color-secondary">{name}</h1>
+                <p className="text-base px-3 font-semibold text-white">{description}</p>
+                <p className="text-lg px-3 font-semibold text-color-secondary">$ {priceFinal}</p>
+            </div>
+            {editProduct}
         </div>
     );
     
