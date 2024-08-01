4dc3b82c4765473f4656f365a47a817f185e0392
Nicolas <nicolasgalarza40@gmail.com>
2024-08-01 00:31:41 -0300

    feat: :sparkles: send mail on reservate for confirm or cancel reservation

--- Backend/src/Service/MailerService.php
+++ Backend/src/Service/MailerService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
@@ -19,6 +20,18 @@ class MailerService
       ->subject($subject)
       ->text($content);
 
+    $this->mailer->send($email);
+  }
+
+  public function sendEmailWithTemplate(string $to, string $subject, string $templatePath, array $context)
+  {
+    $email = (new TemplatedEmail())
+      ->from($_ENV['CUSTOM_EMAIL'])
+      ->to($to)
+      ->subject($subject)
+      ->htmlTemplate($templatePath)
+      ->context($context);
+      
     $this->mailer->send($email);
   }
 }
--- Backend/src/State/ReservationProcessor.php
+++ Backend/src/State/ReservationProcessor.php
@@ -7,13 +7,16 @@ use ApiPlatform\Metadata\Put;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Reservation;
 use App\Repository\ReservationRepository;
+use App\Service\MailerService;
 use App\Service\ReservationService;
+use App\Util\StaticRestaurantInfo;
 
 class ReservationProcessor implements ProcessorInterface
 {
     public function __construct(
         private ReservationRepository $reservationRepository,
-        private ReservationService $reservationService
+        private ReservationService $reservationService,
+        private MailerService $mailerService
     ) {
     }
 
@@ -25,6 +28,27 @@ class ReservationProcessor implements ProcessorInterface
         }
         $code = $this->reservationService->validateAndGenerateCode();
         $reservation = $this->reservationRepository->saveFromDto($data, $code);
+
+        $base64id = base64_encode($reservation->getId());
+        $confirmLink = $_ENV['FRONTEND_URL'] . '/codeConfirmReservation/' . $base64id . '/confirm';
+        $cancelLink = $_ENV['FRONTEND_URL'] . '/codeConfirmReservation/' . $base64id . '/cancel';
+
+        $this->mailerService
+            ->sendEmailWithTemplate(
+                $reservation->getOwnerEmail(), 
+                "Confirmación de reservación en " . StaticRestaurantInfo::NAME, 
+                "reservation.html.twig",
+                [
+                    'code' => $code,
+                    'confirmLink' => $confirmLink,
+                    'cancelLink' => $cancelLink,
+                    'first_name' => $reservation->getOwnerFirstName(),
+                    'last_name' => $reservation->getOwnerLastName(),
+                    'date' => $reservation->getDate()->format('Y-m-d'),
+                    'time' => $reservation->getTime()->format('H:i'),
+                    'table' => $reservation->getTable()->getName(),
+                ]
+            );
 
         return $reservation;
     }
